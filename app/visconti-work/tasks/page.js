7b19511820bac010b1fd1ccf51cc17db1b67efb8
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-09-02 07:36:35 +0200

    feat: add Terna deadline task control view

--- app/visconti-work/tasks/page.js
+++ app/visconti-work/tasks/page.js
@@ -1,12 +1,12 @@
-import ViscontiTaskBoardV2 from "@/components/ViscontiTaskBoardV2";
+import ViscontiTaskBoardTernaV2 from "@/components/ViscontiTaskBoardTernaV2";
 import { getViscontiTaskData } from "@/lib/visconti-task-data";
 
 export const metadata = {
   title: "Attività · Visconti Work V2",
-  description: "Coda operativa di Gruppo Visconti",
+  description: "Coda operativa e scadenze di connessione di Gruppo Visconti",
 };
 
 export default async function TasksPage() {
   const data = await getViscontiTaskData();
-  return <ViscontiTaskBoardV2 {...data} />;
+  return <ViscontiTaskBoardTernaV2 {...data} />;
 }
