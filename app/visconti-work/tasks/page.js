9a6fbacdd985b226915661870290f51649cd71fa
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-09-02 08:32:31 +0200

    feat: make activities page the unified task board

--- app/visconti-work/tasks/page.js
+++ app/visconti-work/tasks/page.js
@@ -1,12 +1,12 @@
-import ViscontiTaskBoardTernaV2 from "@/components/ViscontiTaskBoardTernaV2";
+import ViscontiTaskBoardV2 from "@/components/ViscontiTaskBoardV2";
 import { getViscontiTaskData } from "@/lib/visconti-task-data";
 
 export const metadata = {
   title: "Attività · Visconti Work V2",
-  description: "Coda operativa e scadenze di connessione di Gruppo Visconti",
+  description: "Coda operativa unica di Gruppo Visconti",
 };
 
 export default async function TasksPage() {
   const data = await getViscontiTaskData();
-  return <ViscontiTaskBoardTernaV2 {...data} />;
+  return <ViscontiTaskBoardV2 {...data} />;
 }
