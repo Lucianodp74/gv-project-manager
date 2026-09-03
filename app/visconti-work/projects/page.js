f9e72a1c5e22effc275367a9617a110955c2d499
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-09-03 14:54:23 +0200

    connect project action frame

--- app/visconti-work/projects/page.js
+++ app/visconti-work/projects/page.js
@@ -1,5 +1,6 @@
 import ViscontiProjectDetailV2 from "@/components/ViscontiProjectDetailV2";
 import ViscontiProjectOperationalSummary from "@/components/ViscontiProjectOperationalSummary";
+import ViscontiProjectActionFrame from "@/components/ViscontiProjectActionFrame";
 import ViscontiProjectListV2 from "@/components/ViscontiProjectListV2";
 import { getViscontiWorkData } from "@/lib/visconti-work-data";
 
@@ -14,7 +15,7 @@ export const metadata = {
 export default async function ProjectPage({ searchParams }) {
   const params = await searchParams;
   if (params?.id) {
-    return <><ViscontiProjectOperationalSummary projectId={params.id} /><ViscontiProjectDetailV2 /></>;
+    return <><ViscontiProjectOperationalSummary projectId={params.id} /><ViscontiProjectActionFrame projectId={params.id} /><ViscontiProjectDetailV2 /></>;
   }
   const data = await getViscontiWorkData();
   return <ViscontiProjectListV2 projects={data.projects} connected={data.connected} />;
