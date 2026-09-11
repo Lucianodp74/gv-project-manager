3c4d7f0b8831d2c559de0e3d4e4bebec81851b6d
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-09-11 18:35:56 +0200

    perf: cache control tower page for faster opening

--- app/visconti-work/page.js
+++ app/visconti-work/page.js
@@ -1,8 +1,7 @@
 import ViscontiDirectorControlTower from "@/components/ViscontiDirectorControlTower";
 import { getViscontiControlTowerData } from "@/lib/visconti-control-tower-data";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 15;
 
 export const metadata = {
   title: "Visconti Work V2",
