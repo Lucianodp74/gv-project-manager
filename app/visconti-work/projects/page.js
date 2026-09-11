f11b8c33000fa632776001f89914642f9771f081
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-09-11 11:23:00 +0200

    Use societary workflow for project detail

--- app/visconti-work/projects/page.js
+++ app/visconti-work/projects/page.js
@@ -2,7 +2,7 @@ import ViscontiProjectActionFrame from "@/components/ViscontiProjectActionFrame"
 import ViscontiProjectCompanyCard from "@/components/ViscontiProjectCompanyCard";
 import ViscontiProjectDetailV2 from "@/components/ViscontiProjectDetailV2";
 import ViscontiProjectListV2 from "@/components/ViscontiProjectListV2";
-import ViscontiProjectSpvWorkflow from "@/components/ViscontiProjectSpvWorkflow";
+import ViscontiProjectSocietaryWorkflow from "@/components/ViscontiProjectSocietaryWorkflow";
 import ViscontiNewProject from "@/components/ViscontiNewProject";
 import { getViscontiWorkData } from "@/lib/visconti-work-data";
 
@@ -17,7 +17,7 @@ export const metadata = {
 export default async function ProjectPage({ searchParams }) {
   const params = await searchParams;
   if (params?.id) {
-    return <><ViscontiProjectActionFrame projectId={params.id} /><ViscontiProjectCompanyCard projectId={params.id} /><ViscontiProjectSpvWorkflow projectId={params.id} /><ViscontiProjectDetailV2 /></>;
+    return <><ViscontiProjectActionFrame projectId={params.id} /><ViscontiProjectCompanyCard projectId={params.id} /><ViscontiProjectSocietaryWorkflow projectId={params.id} /><ViscontiProjectDetailV2 /></>;
   }
   const data = await getViscontiWorkData({ includeArchived: true });
   return <><div style={{maxWidth:1480,margin:"0 auto",padding:"18px 34px 0",display:"flex",justifyContent:"flex-end"}}><ViscontiNewProject members={data.members} /></div><ViscontiProjectListV2 projects={data.projects} connected={data.connected} /> </>;
